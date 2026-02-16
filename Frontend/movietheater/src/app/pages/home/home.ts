c29ba3f79b1380aa00e9e3595e5edd7653d906fd
Polawat Krates <fuefolkjj1230@gmail.com>
2026-02-16 15:23:22 +0700

    Setup Project WOWOW

---
+++ Frontend/movietheater/src/app/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Footer } from '../../share/footer/footer';
+import { Navbar } from '../../share/navbar/navbar';
+@Component({
+  selector: 'app-home',
+  imports: [Footer, Navbar],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
