4f52b8f6d16a32e2cf49d798a0ccf55b37c6d086
Polawat Krates <fuefolkjj1230@gmail.com>
2026-02-22 13:18:02 +0700

    ADD FILE CONNECT BACKEND

---
+++ Frontend/movietheater/src/app/admin/dashboard/dashboard.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { MovieService,Movie } from '../../services/movie.service';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
+})
+export class Dashboard {
+movies: Movie[] = [];
+
+  constructor(private MvService: MovieService) {}
+
+  ngOnInit(): void {
+    this.MvService.getAllMovies().subscribe({
+      next: (data) => {
+        console.log('Data from backend:', data);
+        this.movies = data;
+      },
+      error: (err) => {
+        console.error('Error:', err);
+      }
+    });
+  }
+}
--- Frontend/movietheater/src/app/app.routes.ts
+++ Frontend/movietheater/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Dashboard } from './admin/dashboard/dashboard';
 
 export const routes: Routes = [
     {
         path: '',
         component: Home
+    },
+    {
+        path: 'Dashboard',
+        component: Dashboard
     }
 ];
