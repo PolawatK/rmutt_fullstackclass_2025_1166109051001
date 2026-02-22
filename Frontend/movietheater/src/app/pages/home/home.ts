2b1fe11ced7e548e61d34310bd5e0b009cbac623
pongpak726 <fuza2560@gmail.com>
2026-02-22 15:19:37 +0700

    reset html

--- Frontend/movietheater/src/app/pages/home/home.ts
+++ Frontend/movietheater/src/app/pages/home/home.ts
@@ -8,6 +8,23 @@ import { Navbar } from '../../share/navbar/navbar';
   styleUrl: './home.css',
 })
 export class Home {
+scrollRight(){
+
+  }
+  scrollLeft(){
+
+  }
+  
+
+   items1 = [
+    {
+      image: 'assets/images/1.jpg',
+      title: 'Movie 1',
+      description: 'Description of Movie 1'
+    }
+    
+  ]
+
   movies: any[] = [];
 
   constructor() {}
