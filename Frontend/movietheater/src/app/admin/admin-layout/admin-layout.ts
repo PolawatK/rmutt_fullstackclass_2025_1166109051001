d2f9aacbc318ae9c0438a7d4427e7142beceeff7
Polawat Krates <fuefolkjj1230@gmail.com>
2026-02-22 18:43:49 +0700

    Fix error topbar delete

--- Frontend/movietheater/src/app/admin/admin-layout/admin-layout.ts
+++ Frontend/movietheater/src/app/admin/admin-layout/admin-layout.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { Sidenav } from '../../share/sidenav/sidenav';
 import { RouterOutlet } from '@angular/router';
-import { Topbar } from '../../share/topbar/topbar';
 @Component({
   selector: 'app-admin-layout',
-  imports: [Sidenav,RouterOutlet,Topbar],
+  imports: [Sidenav,RouterOutlet],
   templateUrl: './admin-layout.html',
   styleUrl: './admin-layout.css',
 })
