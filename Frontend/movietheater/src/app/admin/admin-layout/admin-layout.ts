b766d92f069db1831feb8c84dbd800126f14c2de
Polawat Krates <fuefolkjj1230@gmail.com>
2026-02-22 18:34:08 +0700

    Update admin layout

--- Frontend/movietheater/src/app/admin/admin-layout/admin-layout.ts
+++ Frontend/movietheater/src/app/admin/admin-layout/admin-layout.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Sidenav } from '../../share/sidenav/sidenav';
 import { RouterOutlet } from '@angular/router';
+import { Topbar } from '../../share/topbar/topbar';
 @Component({
   selector: 'app-admin-layout',
-  imports: [Sidenav,RouterOutlet],
+  imports: [Sidenav,RouterOutlet,Topbar],
   templateUrl: './admin-layout.html',
   styleUrl: './admin-layout.css',
 })
 export class AdminLayout {
 
+ 
 }
