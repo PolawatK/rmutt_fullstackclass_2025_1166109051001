59d1f282b77abc7cade94c19dba9e7ce56f8f41a
Pattarapong01 <itc78816@gmail.com>
2026-02-27 14:27:06 +0700

    create total-booking component in admin module

---
+++ Frontend/movietheater/src/app/admin/total-booking/total-booking.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-total-booking',
+  imports: [],
+  templateUrl: './total-booking.html',
+  styleUrl: './total-booking.css',
+})
+export class TotalBooking {
+
+}
