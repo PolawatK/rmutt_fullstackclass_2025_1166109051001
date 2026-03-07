d16e5b3e7ab6736f20e9dd57d76f6a1e371fed83
Baramee Maprom <BarameeMaprom@gmail.com>
2026-03-07 18:24:24 +0700

    add showtime

---
+++ Frontend/movietheater/src/app/admin/showtime/showtime.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-showtime',
+  imports: [],
+  templateUrl: './showtime.html',
+  styleUrl: './showtime.css',
+})
+export class Showtime {
+
+}
