c7a1aafd9985d1ad51b074082dd665a2650cf330
Pattarapong01 <itc78816@gmail.com>
2026-03-13 19:47:21 +0700

    Add authenticateToken

--- Backend/src/routes/bookingcrud.route.js
+++ Backend/src/routes/bookingcrud.route.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingcrud-controller');
 const { authenticateAdmin } = require('../middleware/authMiddleware');
+const { authenticateToken } = require('../middleware/authMiddleware');
 
-router.get('/', authenticateAdmin,bookingController.getAll);
+
+router.get('/', authenticateToken,authenticateAdmin,bookingController.getAll);
 
 module.exports = router;
--- Backend/src/routes/payment.routes.js
+++ Backend/src/routes/payment.routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const paymentController = require('../controllers/payment-controller');
 const { authenticateAdmin } = require('../middleware/authMiddleware');
+const { authenticateToken } = require('../middleware/authMiddleware');
 
-router.get('/',authenticateAdmin, paymentController.getAllPayments);
+router.get('/',authenticateToken,authenticateAdmin, paymentController.getAllPayments);
 
 module.exports = router;
