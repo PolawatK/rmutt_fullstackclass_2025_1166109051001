8b6f38935e83da29bd9c5241b4d987c3464cea4c
poramet2244 <porametponcha2424@gmail.com>
2026-03-13 19:49:32 +0700

    review

--- Backend/src/routes/review-routes.js
+++ Backend/src/routes/review-routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/review-controller');
-const { authenticateToken } = require('../middleware/authMiddleware');
+
 const { authenticateAdmin,authenticateToken } = require('../middleware/authMiddleware');
 router.get('/', authenticateToken, authenticateAdmin, reviewController.getReviewData);
 router.get('/movie/:movieId', reviewController.getReviewDataByMovie);
