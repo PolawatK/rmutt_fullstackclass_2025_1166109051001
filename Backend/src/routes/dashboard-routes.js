390f42b052852e6ad77966e6f970e0609994638b
Polawat Krates <fuefolkjj1230@gmail.com>
2026-03-13 19:24:59 +0700

    Up authenticate Admin

--- Backend/src/routes/dashboard-routes.js
+++ Backend/src/routes/dashboard-routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controllers/dashboard-controller');
+const { authenticateAdmin } = require('../middleware/authMiddleware');
 
-
-router.get('/', dashboardController.getDashboardData);
+router.get('/',authenticateAdmin, dashboardController.getDashboardData);
 
 module.exports = router;
